6b4cd525cb214190c7f1d6134ee586140009880c
Yonathan Assefa <yab95assefa@gmail.com>
2021-04-01 17:04:19 +0300

    my commit

---
+++ facex/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Student)
+admin.site.register(YearOfStudy)
+admin.site.register(FieldOfStudy)
+admin.site.register(Department)
---
+++ facex/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+
+from django.db import models
+
+class Department(models.Model):
+	department = models.CharField(max_length = 200)
+
+	def __str__(self):
+		return self.department
+
+class FieldOfStudy(models.Model):
+	field_of_study = models.CharField(max_length = 200)
+
+	def __str__(self):
+		return self.field_of_study
+
+class YearOfStudy(models.Model):
+	year_of_study = models.CharField(max_length = 200)
+
+	def __str__(self):
+		return self.year_of_study
+
+
+class Student(models.Model):
+	image = models.ImageField(upload_to = 'images/face_datas')
+	name = models.CharField(max_length = 150)
+	id_n = models.CharField(max_length = 12)
+	
+	dept = models.ForeignKey(Department, on_delete = models.PROTECT)
+	field_of_study = models.ForeignKey(FieldOfStudy, on_delete = models.PROTECT)
+	year_of_study = models.ForeignKey(YearOfStudy, on_delete = models.PROTECT)
+
+	def __str__(self):
+		return self.name
+
+
+
